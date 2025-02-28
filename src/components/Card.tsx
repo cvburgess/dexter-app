368e5a96cd3252f19a3c3ff30afd1e1c7ad23798
Charles Burgess <consulting@cvburgess.com>
2025-02-27 22:26:10 -0500

    Add dynamic styles with Tailwind

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -23,13 +23,51 @@ enum TaskStatus {
 export const Card = (
   { description, priority, status, subtasks, title }: Task,
 ) => {
+  const colors = getColors(priority);
+
   return (
-    <div className="bg-card-bg shadow-md rounded-lg p-4 m-4 w-sm">
-      <h3 className="text-lg font-semibold mb-2">{title}</h3>
-      <p className="text-card-text mb-2">{description}</p>
-      <p className="text-card-text">
+    <div className={`shadow-md rounded-lg p-4 m-4 w-sm ${colors.bg}`}>
+      <h3 className={`text-lg font-semibold mb-2 ${colors.text}`}>
+        {title}
+      </h3>
+      <p className={`mb-2 ${colors.text}`}>{description}</p>
+      <p className={colors.text}>
         Status: {status} | Priority: {priority} | Subtasks: {subtasks.length}
       </p>
     </div>
   );
+};
+
+const getColors = (priority: TaskPriority | null) => {
+  switch (priority) {
+    case TaskPriority.IMPORTANT_AND_URGENT:
+      return {
+        bg: "bg-card-p0-bg",
+        text: "text-card-p0-text",
+      };
+
+    case TaskPriority.IMPORTANT:
+      return {
+        bg: "bg-card-p1-bg",
+        text: "text-card-p1-text",
+      };
+
+    case TaskPriority.URGENT:
+      return {
+        bg: "bg-card-p2-bg",
+        text: "text-card-p2-text",
+      };
+
+    case TaskPriority.NEITHER:
+      return {
+        bg: "bg-card-p3-bg",
+        text: "text-card-p3-text",
+      };
+
+    default:
+      return {
+        bg: "bg-card-none-bg",
+        text: "text-card-none-text",
+      };
+  }
 };
