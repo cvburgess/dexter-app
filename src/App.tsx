09e5fc1e60e4780d38d745eb7fae773d7b63242a
Charles Burgess <consulting@cvburgess.com>
2025-02-27 21:10:11 -0500

    Refactor theme and card data

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,50 @@ import "./App.css";
 import { Card } from "./components/Card.tsx";
 
 const tasks = [
-  { id: "001-abc-000", status: 0, priority: 0 },
-  { id: "002-abc-000", status: 1, priority: 1 },
-  { id: "003-abc-000", status: 2, priority: 1 },
-  { id: "004-abc-000", status: 3, priority: 3 },
-  { id: "005-abc-000", status: 1, priority: null },
-  { id: "006-abc-000", status: 0, priority: null },
+  {
+    id: "001-abc-000",
+    status: 0,
+    priority: 0,
+    title: "Complete first task",
+    subtasks: [],
+  },
+  {
+    id: "002-abc-000",
+    status: 1,
+    priority: 1,
+    title: "Make another task",
+    subtasks: [],
+  },
+  {
+    id: "003-abc-000",
+    description: "All files in Dropbox",
+    status: 2,
+    priority: 1,
+    title: "File taxes",
+    subtasks: [],
+  },
+  {
+    id: "004-abc-000",
+    status: 3,
+    priority: 3,
+    title: "Take out the trash",
+    subtasks: [],
+  },
+  {
+    id: "005-abc-000",
+    status: 1,
+    priority: null,
+    title: "Build the app",
+    subtasks: [],
+  },
+  {
+    id: "006-abc-000",
+    description: "Recruiter is Angela",
+    status: 0,
+    priority: null,
+    title: "Submit resume",
+    subtasks: [],
+  },
 ];
 
 function App() {
@@ -15,9 +53,12 @@ function App() {
     <main className="">
       {tasks.map((task) => (
         <Card
+          description={task.description}
           key={task.id}
           priority={task.priority}
           status={task.status}
+          title={task.title}
+          subtasks={task.subtasks}
         />
       ))}
     </main>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,9 @@
 type Task = {
+  description?: string;
   priority: TaskPriority | null;
   status: TaskStatus;
+  subtasks: Task[];
+  title: string;
 };
 
 enum TaskPriority {
@@ -17,11 +20,16 @@ enum TaskStatus {
   WONT_DO,
 }
 
-export const Card = ({ priority, status }: Task) => {
+export const Card = (
+  { description, priority, status, subtasks, title }: Task,
+) => {
   return (
-    <div className="bg-white shadow-md rounded-lg p-4 m-4 w-sm">
-      <h3 className="text-lg font-semibold mb-2">Card Title</h3>
-      <p className="text-gray-700">Status: {status} | Priority: {priority}</p>
+    <div className="bg-card-bg shadow-md rounded-lg p-4 m-4 w-sm">
+      <h3 className="text-lg font-semibold mb-2">{title}</h3>
+      <p className="text-card-text mb-2">{description}</p>
+      <p className="text-card-text">
+        Status: {status} | Priority: {priority} | Subtasks: {subtasks.length}
+      </p>
     </div>
   );
 };
