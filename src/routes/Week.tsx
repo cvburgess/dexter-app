48f576327995881eddee94def9f611a8354041ec
Charles Burgess <consulting@cvburgess.com>
2025-03-12 18:36:39 -0400

    Fix base filters for week quick planner

--- src/routes/Week.tsx
+++ src/routes/Week.tsx
@@ -5,9 +5,9 @@ import { Board, TColumn } from "../components/Board.tsx";
 import { QuickPlanner } from "../components/QuickPlanner.tsx";
 import { View } from "../components/View.tsx";
 
-import { useTasks } from "../hooks/useTasks.tsx";
+import { taskFilters, useTasks } from "../hooks/useTasks.tsx";
 import { TTask } from "../api/tasks.ts";
-import { TQueryFilter } from "../api/applyFilters.ts";
+import { makeOrFilter, TQueryFilter } from "../api/applyFilters.ts";
 import { weekStartEnd } from "../utils/weekStartEnd.ts";
 
 export const Week = () => {
@@ -21,8 +21,11 @@ export const Week = () => {
       ["scheduledFor", "lte", sunday.toString()],
     ],
     notThisWeek: [
-      ["scheduledFor", "lt", mostRecentMonday.toString()],
-      ["scheduledFor", "gt", sunday.toString()],
+      makeOrFilter([
+        ["scheduledFor", "lt", mostRecentMonday.toString()],
+        ["scheduledFor", "gt", sunday.toString()],
+      ]),
+      ...taskFilters.incomplete,
     ],
   };
 
