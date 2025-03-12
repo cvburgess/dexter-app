a58aacc46caec71c305e154a24801dd4acf1909d
Charles Burgess <consulting@cvburgess.com>
2025-03-12 16:05:33 -0400

    WIP

--- src/components/QuickPlanner.tsx
+++ src/components/QuickPlanner.tsx
@@ -6,10 +6,12 @@ import { taskFilters, useTasks } from "../hooks/useTasks.tsx";
 
 import { Column } from "./Column.tsx";
 import { InputWithIcon } from "./InputWithIcon.tsx";
+import classNames from "classnames";
 
 export const QuickPlanner = () => {
   const [search, setSearch] = useState<string>("");
   const [filteredTasks] = useTasks(taskFilters.unscheduled);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const searchTasks = (tasks: TTask[]) =>
     tasks.filter((task): boolean =>
@@ -17,57 +19,72 @@ export const QuickPlanner = () => {
     );
 
   return (
-    <div className="fixed p-4 overflow-x-hidden overflow-y-scroll top-0 bottom-0 right-0 z-100 bg-base-100 shadow-[-8px_0px_8px_0px_rgba(0,0,0,0.1)]">
-      <div className="join max-w-70">
-        <div className="dropdown">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn join-item p-4 h-[51px] bg-base-300 border-none text-xs"
-          >
-            Unscheduled
-          </div>
-          <ul
-            tabIndex={0}
-            className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
-          >
-            <li>
-              <a className="bg-base-300">
-                <span>📆</span>
-                <span>Unscheduled</span>
-              </a>
-            </li>
-            <li>
-              <a>
-                <span>⌛</span>
-                <span>Overdue</span>
-              </a>
-            </li>
-            <li>
-              <a>
-                <span>⏳</span>
-                <span>Due soon</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        <InputWithIcon
-          // className="join-item"
-          type="text"
-          placeholder="Search"
-          onChange={(event) => setSearch(event.target.value)}
-          wrapperClassName="join-item"
-          value={search}
-        >
-          <MagnifyingGlass />
-        </InputWithIcon>
+    <div className="fixed top-0 bottom-0 right-0 z-100 flex">
+      <div
+        className="p-2 bg-base-300 border-none text-xs"
+        onClick={() => setIsOpen(!isOpen)}
+        style={{ alignSelf: "center" }}
+      >
+        {isOpen ? ">" : "<"}
       </div>
-      <Column
-        // cardSize="compact-h"
-        id="scheduledFor:null"
-        tasks={searchTasks(filteredTasks)}
-      />
+
+      <div
+        className={classNames(
+          "overflow-x-hidden overflow-y-scroll bg-base-100 shadow-[-8px_0px_8px_0px_rgba(0,0,0,0.1)] transition-all duration-3000",
+          isOpen ? "w-auto p-4" : "w-0 p-0",
+        )}
+      >
+        <div className="join max-w-70">
+          <div className="dropdown">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn join-item p-4 h-[51px] bg-base-300 border-none text-xs"
+            >
+              Unscheduled
+            </div>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+            >
+              <li>
+                <a className="bg-base-300">
+                  <span>📆</span>
+                  <span>Unscheduled</span>
+                </a>
+              </li>
+              <li>
+                <a>
+                  <span>⌛</span>
+                  <span>Overdue</span>
+                </a>
+              </li>
+              <li>
+                <a>
+                  <span>⏳</span>
+                  <span>Due soon</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <InputWithIcon
+            // className="join-item"
+            type="text"
+            placeholder="Search"
+            onChange={(event) => setSearch(event.target.value)}
+            wrapperClassName="join-item"
+            value={search}
+          >
+            <MagnifyingGlass />
+          </InputWithIcon>
+        </div>
+        <Column
+          // cardSize="compact-h"
+          id="scheduledFor:null"
+          tasks={searchTasks(filteredTasks)}
+        />
+      </div>
     </div>
   );
 };
