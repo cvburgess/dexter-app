f3d2c06f10a0c8daddc1a2df36947a8d8d9db190
Charles Burgess <consulting@cvburgess.com>
2025-03-14 17:02:08 -0400

    Add carousel

--- src/routes/Review.tsx
+++ src/routes/Review.tsx
@@ -16,6 +16,36 @@ export const Review = () => {
   return (
     <View className="flex-col">
       <DayNav date={date} setDate={setDate} />
+
+      <div className="carousel carousel-center rounded-box w-full h-full space-x-8 p-8">
+        <CarouselItem
+          title="Reflect"
+          subtitle="Take a deep breath and check in with yourself"
+        />
+        <CarouselItem
+          title="Review"
+          subtitle="Look back at the day and decide what to do next"
+        />
+        <CarouselItem
+          title="Plan"
+          subtitle="Make time and space for the things that matter most"
+        />
+      </div>
     </View>
   );
 };
+
+type TCarouselItemProps = {
+  subtitle: string;
+  title: string;
+};
+
+const CarouselItem = ({ subtitle, title }: TCarouselItemProps) => (
+  <div id="slide1" className="carousel-item">
+    <div className="flex flex-col rounded-box w-[80vw] border-1 border-base-200 shadow-xl items-center justify-center p-8">
+      <h1 className="text-4xl font-black opacity-80">{title}</h1>
+      <p className="text-xs italic opacity-40 mt-2 mb-4">{subtitle}</p>
+      <div className="flex w-full min-h-[66%]" />
+    </div>
+  </div>
+);
