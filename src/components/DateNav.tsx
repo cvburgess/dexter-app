797855a52eb361eaa0841cce7f71b997da63974c
Charles Burgess <consulting@cvburgess.com>
2025-03-14 12:35:26 -0400

    Refactor to a component

---
+++ src/components/DateNav.tsx
@@ -0,0 +1,73 @@
+import { Temporal } from "@js-temporal/polyfill";
+import { CaretLeft, CaretRight } from "@phosphor-icons/react";
+
+import { ButtonWithPopover } from "../components/ButtonWithPopover.tsx";
+
+type TDateNavProps = {
+  date: Temporal.PlainDate;
+  setDate: (date: Temporal.PlainDate) => void;
+  mode: "days" | "weeks";
+};
+
+export const DateNav = ({ date, setDate }: TDateNavProps) => {
+  return (
+    <div className="flex items-center justify-center p-4 pb-0 w-full">
+      <div
+        className="btn btn-ghost"
+        onClick={() => setDate(date.subtract({ days: 1 }))}
+      >
+        <CaretLeft />
+      </div>
+
+      {date.toString() === Temporal.Now.plainDateISO().toString()
+        ? (
+          <ButtonWithPopover
+            buttonClassName="btn btn-ghost min-w-50"
+            buttonVariant="none"
+            onChange={(value) =>
+              value && setDate(Temporal.PlainDate.from(value))}
+            selectedDate={date.toString()}
+            variant="calendar"
+            wrapperClassName="dropdown-center"
+          >
+            {getRelativeDay(date)}
+          </ButtonWithPopover>
+        )
+        : (
+          <div
+            className="btn btn-ghost min-w-50"
+            onClick={() => setDate(Temporal.Now.plainDateISO())}
+          >
+            {getRelativeDay(date)}
+          </div>
+        )}
+
+      <div
+        className="btn btn-ghost"
+        onClick={() => setDate(date.add({ days: 1 }))}
+      >
+        <CaretRight />
+      </div>
+    </div>
+  );
+};
+
+const getRelativeDay = (date: Temporal.PlainDate) => {
+  const today = Temporal.Now.plainDateISO();
+  const diffDays = today.until(date).days;
+
+  switch (diffDays) {
+    case 0:
+      return "Today";
+    case 1:
+      return "Tomorrow";
+    case -1:
+      return "Yesterday";
+    default:
+      return date.toLocaleString("en-US", {
+        weekday: "short",
+        month: "short",
+        day: "numeric",
+      });
+  }
+};
--- src/routes/Today.tsx
+++ src/routes/Today.tsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
-import { CaretLeft, CaretRight } from "@phosphor-icons/react";
 import { Temporal } from "@js-temporal/polyfill";
 
 import { Board } from "../components/Board.tsx";
-import { ButtonWithPopover } from "../components/ButtonWithPopover.tsx";
 import { View } from "../components/View.tsx";
 
 import { taskFilters, useTasks } from "../hooks/useTasks.tsx";
 import { QuickPlanner } from "../components/QuickPlanner.tsx";
+import { DateNav } from "../components/DateNav.tsx";
 
 export const Today = () => {
   const [date, setDate] = useState<Temporal.PlainDate>(
@@ -17,45 +16,7 @@ export const Today = () => {
 
   return (
     <View className="flex-col">
-      <div className="flex items-center justify-center p-4 pb-0 w-full">
-        <div
-          className="btn btn-ghost"
-          onClick={() => setDate(date.subtract({ days: 1 }))}
-        >
-          <CaretLeft />
-        </div>
-
-        {date.toString() === Temporal.Now.plainDateISO().toString()
-          ? (
-            <ButtonWithPopover
-              buttonClassName="btn btn-ghost min-w-50"
-              buttonVariant="none"
-              onChange={(value) =>
-                value && setDate(Temporal.PlainDate.from(value))}
-              selectedDate={date.toString()}
-              variant="calendar"
-              wrapperClassName="dropdown-center"
-            >
-              {getRelativeDay(date)}
-            </ButtonWithPopover>
-          )
-          : (
-            <div
-              className="btn btn-ghost min-w-50"
-              onClick={() => setDate(Temporal.Now.plainDateISO())}
-            >
-              {getRelativeDay(date)}
-            </div>
-          )}
-
-        <div
-          className="btn btn-ghost"
-          onClick={() => setDate(date.add({ days: 1 }))}
-        >
-          <CaretRight />
-        </div>
-      </div>
-
+      <DateNav date={date} setDate={setDate} mode="days" />
       <Board
         canCreateTasks
         columns={[{
@@ -68,24 +29,4 @@ export const Today = () => {
       <QuickPlanner baseFilters={taskFilters.notToday} />
     </View>
   );
-};
-
-const getRelativeDay = (date: Temporal.PlainDate) => {
-  const today = Temporal.Now.plainDateISO();
-  const diffDays = today.until(date).days;
-
-  switch (diffDays) {
-    case 0:
-      return "Today";
-    case 1:
-      return "Tomorrow";
-    case -1:
-      return "Yesterday";
-    default:
-      return date.toLocaleString("en-US", {
-        weekday: "short",
-        month: "short",
-        day: "numeric",
-      });
-  }
 };
