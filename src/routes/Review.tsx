b21cbed1b98e39003a4b3448fd657a6cf3e8566a
Charles Burgess <consulting@cvburgess.com>
2025-03-14 22:27:08 -0400

    Update Review.tsx

--- src/routes/Review.tsx
+++ src/routes/Review.tsx
@@ -106,7 +106,7 @@ const CardListWithTitle = ({ tasks, variant }: TCardListWithTItleProps) => {
   const Icon = variant === "complete" ? CheckCircle : XCircle;
 
   return (
-    <div className="flex flex-col items-center gap-4">
+    <div className="flex flex-col items-center gap-4 min-w-70">
       <p className="text-xl font-bold opacity-60 inline-flex items-center pr-5 mt-4 capitalize">
         <Icon
           className={classNames("mr-4", {
