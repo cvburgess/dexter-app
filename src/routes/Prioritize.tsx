f706e7666cf11149b829182a349481c55ce6b025
Charles Burgess <consulting@cvburgess.com>
2025-03-17 14:22:47 -0400

    Use eslint to format files on save

--- src/routes/Prioritize.tsx
+++ src/routes/Prioritize.tsx
@@ -12,11 +12,7 @@ export const Prioritize = () => {
 
   return (
     <View>
-      <Board
-        canCreateTasks
-        columns={columns}
-        groupBy="priority"
-      />
+      <Board canCreateTasks columns={columns} groupBy="priority" />
     </View>
   );
 };
@@ -26,36 +22,36 @@ const makeColumns = (tasks: TTask[] | undefined = []): TColumn[] => [
     autoCollapse: true,
     id: ETaskPriority.UNPRIORITIZED.toString(),
     title: "Unprioritized",
-    tasks: tasks?.filter((task: TTask) =>
-      task.priority === ETaskPriority.UNPRIORITIZED,
+    tasks: tasks?.filter(
+      (task: TTask) => task.priority === ETaskPriority.UNPRIORITIZED
     ),
   },
   {
     id: ETaskPriority.IMPORTANT_AND_URGENT.toString(),
     title: "Important and Urgent",
-    tasks: tasks?.filter((task: TTask) =>
-      task.priority === ETaskPriority.IMPORTANT_AND_URGENT,
+    tasks: tasks?.filter(
+      (task: TTask) => task.priority === ETaskPriority.IMPORTANT_AND_URGENT
     ),
   },
   {
     id: ETaskPriority.IMPORTANT.toString(),
     title: "Important",
-    tasks: tasks?.filter((task: TTask) =>
-      task.priority === ETaskPriority.IMPORTANT,
+    tasks: tasks?.filter(
+      (task: TTask) => task.priority === ETaskPriority.IMPORTANT
     ),
   },
   {
     id: ETaskPriority.URGENT.toString(),
     title: "Urgent",
-    tasks: tasks?.filter((task: TTask) =>
-      task.priority === ETaskPriority.URGENT,
+    tasks: tasks?.filter(
+      (task: TTask) => task.priority === ETaskPriority.URGENT
     ),
   },
   {
     id: ETaskPriority.NEITHER.toString(),
     title: "Rainy Day",
-    tasks: tasks?.filter((task: TTask) =>
-      task.priority === ETaskPriority.NEITHER,
+    tasks: tasks?.filter(
+      (task: TTask) => task.priority === ETaskPriority.NEITHER
     ),
   },
 ];
