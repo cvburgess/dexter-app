446f17ec398692ae3a7149b8f455c3406177652d
Charles Burgess <consulting@cvburgess.com>
2025-03-21 11:35:19 -0400

    Update Settings.tsx

--- src/routes/Settings.tsx
+++ src/routes/Settings.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import classNames from "classnames";
 
 import { TextToolbar } from "../components/Toolbar.tsx";
 import { ScrollableContainer, View } from "../components/View.tsx";
@@ -14,10 +15,15 @@ export const Settings = () => {
     <View>
       <TextToolbar title="Settings" />
       <ScrollableContainer>
-        <ul className="menu bg-base-100 rounded-box w-56 mt-4">
+        <ul className="menu bg-base-100 rounded-box w-standard mt-4">
           {panels.map((panel) => (
-            <li key={panel}>
-              <a onClick={() => setActivePanel(panel)}>{panel}</a>
+            <li className="my-1" key={panel}>
+              <a
+                className={classNames({ "bg-base-200": activePanel === panel })}
+                onClick={() => setActivePanel(panel)}
+              >
+                {panel}
+              </a>
             </li>
           ))}
         </ul>
@@ -30,7 +36,7 @@ export const Settings = () => {
 
 const Panel = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="border-2 border-base-200 w-full h-[calc(100vh-5.5rem)] rounded-box mt-4 p-4">
+    <div className="border-2 border-base-200 w-full h-[calc(100vh-5.5rem)] rounded-box mt-4 p-4 overflow-auto">
       {children}
     </div>
   );
@@ -67,10 +73,12 @@ const Theme = () => {
 
   return (
     <Panel>
-      <div className="flex flex-wrap gap-4">
+      <div className="grid grid-cols-[repeat(auto-fill,minmax(160px,1fr))] gap-4 mb-4">
         {lightThemes.map((theme) => (
           <ThemeOption theme={theme} />
         ))}
+      </div>
+      <div className="grid grid-cols-[repeat(auto-fill,minmax(160px,1fr))] gap-4">
         {darkThemes.map((theme) => (
           <ThemeOption theme={theme} />
         ))}
@@ -80,28 +88,28 @@ const Theme = () => {
 };
 
 const ThemeOption = ({ theme }: { theme: (typeof THEMES)[number] }) => (
-  <div className="w-compact border-base-content/20 hover:border-base-content/40 overflow-hidden rounded-lg border">
+  <div className="min-w-compact flex-1 border-1 border-base-content/20 hover:border-base-content/40 overflow-hidden rounded-box">
     <div
       className="bg-base-100 text-base-content w-full cursor-pointer font-sans"
       data-theme={theme.name}
     >
-      <div className="grid grid-cols-5 grid-rows-3">
-        <div className="bg-base-200 col-start-1 row-span-2 row-start-1"></div>
-        <div className="bg-base-300 col-start-1 row-start-3"></div>
-        <div className="bg-base-100 col-span-4 col-start-2 row-span-3 row-start-1 flex flex-col gap-1 p-2">
+      <div className="grid grid-cols-5 grid-rows-2">
+        <div className="bg-base-200 col-start-1 row-span-1 row-start-1"></div>
+        <div className="bg-base-300 col-start-1 row-start-2"></div>
+        <div className="bg-base-100 col-span-4 col-start-2 row-span-2 row-start-1 flex flex-col gap-1 p-2">
           <div className="font-bold">{theme.name}</div>
           <div className="flex flex-wrap gap-1">
-            <div className="bg-primary flex aspect-square w-5 items-center justify-center rounded lg:w-6">
-              <div className="text-primary-content text-sm font-bold">A</div>
+            <div className="bg-base-content/80 flex aspect-square w-6 items-center justify-center rounded">
+              <div className="text-base-100 text-sm font-bold">A</div>
             </div>
-            <div className="bg-secondary flex aspect-square w-5 items-center justify-center rounded lg:w-6">
-              <div className="text-secondary-content text-sm font-bold">A</div>
+            <div className="bg-warning flex aspect-square w-6 items-center justify-center rounded">
+              <div className="text-warning-content text-sm font-bold">A</div>
             </div>
-            <div className="bg-accent flex aspect-square w-5 items-center justify-center rounded lg:w-6">
-              <div className="text-accent-content text-sm font-bold">A</div>
+            <div className="bg-error flex aspect-square w-6 items-center justify-center rounded">
+              <div className="text-error-content text-sm font-bold">A</div>
             </div>
-            <div className="bg-neutral flex aspect-square w-5 items-center justify-center rounded lg:w-6">
-              <div className="text-neutral-content text-sm font-bold">A</div>
+            <div className="bg-info flex aspect-square w-6 items-center justify-center rounded">
+              <div className="text-info-content text-sm font-bold">A</div>
             </div>
           </div>
         </div>
