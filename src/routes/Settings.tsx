8c4ac48c8b156088ceafcf08b0590ee8b4c3236a
Charles Burgess <consulting@cvburgess.com>
2025-03-21 10:21:13 -0400

    WIP panel layout

--- src/routes/Settings.tsx
+++ src/routes/Settings.tsx
@@ -1,14 +1,44 @@
+import { useState } from "react";
+
 import { TextToolbar } from "../components/Toolbar.tsx";
-import { View } from "../components/View.tsx";
+import { ScrollableContainer, View } from "../components/View.tsx";
 
 import { useAuth } from "../hooks/useAuth.tsx";
 
 export const Settings = () => {
-  const { signOut } = useAuth();
+  const [activePanel, setActivePanel] = useState<string>("Account");
+  const panels = ["Account", "Theme", "About"];
 
   return (
     <View>
       <TextToolbar title="Settings" />
+      <ScrollableContainer>
+        <ul className="menu bg-base-100 rounded-box w-56 mt-4">
+          {panels.map((panel) => (
+            <li key={panel}>
+              <a onClick={() => setActivePanel(panel)}>{panel}</a>
+            </li>
+          ))}
+        </ul>
+        {activePanel === "Account" && <Account />}
+      </ScrollableContainer>
+    </View>
+  );
+};
+
+const Panel = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="border-2 border-base-200 w-full h-[calc(100vh-5.5rem)] rounded-box mt-4 p-4">
+      {children}
+    </div>
+  );
+};
+
+const Account = () => {
+  const { signOut } = useAuth();
+
+  return (
+    <Panel>
       <button
         type="button"
         className="btn"
@@ -16,6 +46,13 @@ export const Settings = () => {
       >
         Sign out
       </button>
-    </View>
+      <button
+        type="button"
+        className="btn"
+        onClick={async () => await signOut()}
+      >
+        Delete account
+      </button>
+    </Panel>
   );
 };
