e714eb781998c034f44f6b5ea54a9332d0a3a38b
Charles Burgess <consulting@cvburgess.com>
2025-03-25 15:05:27 -0400

    Add logic to show/hide features based on settings

--- src/routes/Today.tsx
+++ src/routes/Today.tsx
@@ -11,15 +11,16 @@ import {
   ScrollableContainer,
 } from "../components/View.tsx";
 
+import { usePreferences } from "../hooks/usePreferences.tsx";
 import { taskFilters, useTasks } from "../hooks/useTasks.tsx";
 
 export const Today = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-
   const [date, setDate] = useState<Temporal.PlainDate>(
     Temporal.Now.plainDateISO(),
   );
 
+  const [{ enableJournal, enableNotes }] = usePreferences();
   const [tasks] = useTasks([["scheduledFor", "eq", date.toString()]]);
 
   return (
@@ -37,26 +38,36 @@ export const Today = () => {
             id={`scheduledFor:${date.toString()}`}
             tasks={tasks}
           />
-          <div className="tabs tabs-lift py-4 h-[calc(100vh-6rem)] sticky top-0 w-full">
-            <input
-              aria-label="Notes"
-              className="tab"
-              defaultChecked
-              name="today-tabs"
-              type="radio"
-            />
-            <div className="tab-content bg-base-100 border-base-300 p-4 min-w-standard h-full"></div>
-
-            <input
-              aria-label="Journal"
-              className="tab"
-              name="today-tabs"
-              type="radio"
-            />
-            <div className="tab-content bg-base-100 border-base-300 p-4 min-w-standard h-full">
-              <Journal date={date} />
+          {(enableNotes || enableJournal) && (
+            <div className="tabs tabs-lift py-4 h-[calc(100vh-6rem)] sticky top-0 w-full">
+              {enableNotes && (
+                <>
+                  <input
+                    aria-label="Notes"
+                    className="tab"
+                    defaultChecked
+                    name="today-tabs"
+                    type="radio"
+                  />
+                  <div className="tab-content bg-base-100 border-base-300 p-4 min-w-standard h-full"></div>
+                </>
+              )}
+              {enableJournal && (
+                <>
+                  <input
+                    aria-label="Journal"
+                    className="tab"
+                    defaultChecked={!enableNotes}
+                    name="today-tabs"
+                    type="radio"
+                  />
+                  <div className="tab-content bg-base-100 border-base-300 p-4 min-w-standard h-full">
+                    <Journal date={date} />
+                  </div>
+                </>
+              )}
             </div>
-          </div>
+          )}
         </ScrollableContainer>
         <QuickDrawer baseFilters={taskFilters.notToday} isOpen={isOpen} />
       </DrawerContainer>
