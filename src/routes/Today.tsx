31b0f130364f3e9ef063c277668adbbb55d9a1b4
Charles Burgess <consulting@cvburgess.com>
2025-03-25 23:03:10 -0400

    Refactor Tabs to components

--- src/routes/Today.tsx
+++ src/routes/Today.tsx
@@ -39,39 +39,60 @@ export const Today = () => {
             id={`scheduledFor:${date.toString()}`}
             tasks={tasks}
           />
-          {(enableNotes || enableJournal) && (
-            <div className="tabs tabs-lift py-4 h-[calc(100vh-6rem)] sticky top-0 w-full">
-              {enableNotes && (
-                <>
-                  <input
-                    aria-label="Notes"
-                    className="tab"
-                    defaultChecked
-                    name="today-tabs"
-                    type="radio"
-                  />
-                  <div className="tab-content bg-base-100 border-base-300 p-4 min-w-standard h-full"></div>
-                </>
-              )}
-              {enableJournal && (
-                <>
-                  <input
-                    aria-label="Journal"
-                    className="tab"
-                    defaultChecked={!enableNotes}
-                    name="today-tabs"
-                    type="radio"
-                  />
-                  <div className="tab-content bg-base-100 border-base-300 p-4 min-w-standard h-full">
-                    <Journal date={date} />
-                  </div>
-                </>
-              )}
-            </div>
-          )}
+          <Tabs enabled={enableNotes || enableJournal}>
+            <Tab defaultChecked enabled={enableNotes} title="Notes">
+              {""}
+            </Tab>
+            <Tab
+              defaultChecked={!enableNotes}
+              enabled={enableJournal}
+              title="Journal"
+            >
+              <Journal date={date} />
+            </Tab>
+          </Tabs>
         </ScrollableContainer>
         <QuickDrawer baseFilters={taskFilters.notToday} isOpen={isOpen} />
       </DrawerContainer>
     </DraggableView>
+  );
+};
+
+type TTabsProps = {
+  children: React.ReactNode;
+  enabled: boolean;
+};
+
+const Tabs = ({ children, enabled }: TTabsProps) => {
+  if (!enabled) return null;
+  return (
+    <div className="tabs tabs-lift py-4 h-[calc(100vh-6rem)] sticky top-0 w-full">
+      {children}
+    </div>
+  );
+};
+
+type TTabProps = {
+  children: React.ReactNode;
+  defaultChecked: boolean;
+  enabled: boolean;
+  title: string;
+};
+
+const Tab = ({ children, defaultChecked, enabled, title }: TTabProps) => {
+  if (!enabled) return null;
+  return (
+    <>
+      <input
+        aria-label={title}
+        className="tab"
+        defaultChecked={defaultChecked}
+        name="today-tabs"
+        type="radio"
+      />
+      <div className="tab-content bg-base-100 border-base-300 p-4 min-w-standard h-full">
+        {children}
+      </div>
+    </>
   );
 };
