942e8520168bd3b2af5fa20101423c29326b0b07
Charles Burgess <consulting@cvburgess.com>
2025-04-02 18:39:12 -0400

    v0.12 - Add AGPLv3 License (#43)
    * Add AGPLv3 license
    * Fix missing Supabase env vars breaking builds
    * Remove unused scripts from package.json

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,5 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ["path"],
+    },
+  },
   plugins: [react(), tailwindcss()],
 });
