090bb9d1238d098a008df6777d17d87c39b9d26b
Charles Burgess <consulting@cvburgess.com>
2025-04-14 17:02:44 -0400

    Update Notes.tsx

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -25,23 +25,3 @@ export const Notes = ({ date }: TNotesProps) => {
     />
   );
 };
-
-// # Heading h1
-
-// This is a \`Markdown\` file.
-
-// ## This is a Heading h2
-
-// This is the text under it
-
-// ## Emphasis
-
-// *This text will be italic*
-// _This will also be italic_
-
-// **This text will be bold**
-// __This will also be bold__
-
-// _You **can** combine them_
-
-// ~~You can change your mind, too~~
