681135e45228191f044dd25d889d55ad75e9588b
Charles Burgess <consulting@cvburgess.com>
2025-04-20 10:48:53 -0400

    Add localStorage persistence

--- src/hooks/useCardSize.tsx
+++ src/hooks/useCardSize.tsx
@@ -1,12 +1,17 @@
-import { useState } from "react";
+import { useLocation } from "react-router";
+import { useLocalStorage } from "@uidotdev/usehooks";
 
 import { ECardSize } from "../components/Card";
 
 type TUseCardSize = [ECardSize, () => void];
 
 export const useCardSize = (defaultCardSize: ECardSize): TUseCardSize => {
+  const { pathname } = useLocation();
   // Treat web as fullscreen because it has no window chrome
-  const [cardSize, setCardSize] = useState<ECardSize>(defaultCardSize);
+  const [cardSize, setCardSize] = useLocalStorage<ECardSize>(
+    `cardSize:${pathname}`,
+    defaultCardSize,
+  );
 
   const toggleCardSize = () =>
     setCardSize(
